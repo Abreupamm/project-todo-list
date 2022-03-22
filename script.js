4b118114ffd292912e8740a7fe2668c09084461b
pamela <abreupamm@gmail.com>
2022-03-22 18:25:02 -0300

    desafios 8 feito

--- script.js
+++ script.js
@@ -8,14 +8,19 @@ function criarTarefa() {
   lista.appendChild(tarefa);
   tarefa1.value = '';
 }
-
 const button = document.getElementById('criar-tarefa');
 button.addEventListener('click', criarTarefa);
 
 function mudaCor(event) {
+  const temClase = document.getElementsByClassName('cor');
+  if (temClase.length > 0) {
+    for (let i = 0; i < temClase.length; i += 1) {
+      temClase[i].classList = '';
+    }
+  }
   const selected = event.target;
-  selected.classList = '';
   selected.classList.add('cor');
 }
+
 const tarefaSelected = document.getElementById('lista-tarefas');
 tarefaSelected.addEventListener('click', mudaCor);
