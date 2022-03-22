4e0dbaeefd51acaf227317fa49bd788546f7de78
pamela <abreupamm@gmail.com>
2022-03-22 15:49:59 -0300

    desafios 5 e 6 feitos

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-function criarTarefa () {
+function criarTarefa() {
+  const tarefa = document.createElement('li');
+  const tarefa1 = document.querySelector('#texto-tarefa');
+  const texto = tarefa1.value;
+  tarefa.innerText = texto;
 
+  const lista = document.getElementById('lista-tarefas');
+  lista.appendChild(tarefa);
+  tarefa1.value = '';
 }
+
 const button = document.getElementById('criar-tarefa');
 button.addEventListener('click', criarTarefa);
